14e23009790c0694c0a255446159bea2baf979cf
soyeon <uyyu1734@gmail.com>
2024-05-29 08:26:59 +0900

    feat: userProfile 라우터에 연결 (안되어있었네용)

--- src/routes/userProfile.ts
+++ src/routes/userProfile.ts
@@ -1,6 +1,7 @@
 import express, { NextFunction, Request, Response } from "express";
+import { follow, getUserProfile } from "../controllers/userProfile";
 
 export default (router: express.Router) => {
-  router.get("/userprofile/:userId");
-  router.post("/userprofile/follow/:userId");
+  router.get("/userprofile/:userId", getUserProfile);
+  router.post("/userprofile/follow/:userId", follow);
 };
