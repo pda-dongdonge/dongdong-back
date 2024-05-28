6e8e0f2e49e3a4d5b4434e5fcb603b86654c25a3
soyeon <uyyu1734@gmail.com>
2024-05-28 16:23:49 +0900

    #8 feat: userprofile 모델생성

---
+++ src/models/UserProfile.ts
@@ -0,0 +1,41 @@
+import mongoose, { Document, Schema, Model } from "mongoose";
+
+const userSchema = new Schema({
+  userId: {
+    type: mongoose.Types.ObjectId,
+    ref: "user",
+    required: true,
+  },
+  followers: {
+    type: [mongoose.Schema.Types.ObjectId],
+    required: true,
+    ref: "user",
+  },
+  following: {
+    type: [mongoose.Schema.Types.ObjectId],
+    required: true,
+    ref: "user",
+  },
+  likedBucket: {
+    type: [mongoose.Schema.Types.ObjectId],
+    required: true,
+    ref: "bucket",
+  },
+});
+
+export const UserProfileModel = mongoose.model("UserProfile", userSchema);
+
+export const getUserProfiles = () => UserProfileModel.find();
+export const getUserProfileById = (userId: string) =>
+  UserProfileModel.findOne({ userId });
+export const createUserProfile = (values: Record<string, any>) =>
+  new UserProfileModel(values)
+    .save()
+    .then((user) => user.toObject())
+    .catch((err) => {
+      return err;
+    });
+export const deleteUserById = (userId: string) =>
+  UserProfileModel.findOneAndDelete({ userId: userId });
+export const updateUserById = (userId: string, values: Record<string, any>) =>
+  UserProfileModel.findByIdAndUpdate(userId, values);
